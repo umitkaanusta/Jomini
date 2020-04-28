b4398e920987a74c1479ea0f3c65c17439fa9018
Ümit Kaan Usta <53405015+umitkaanusta@users.noreply.github.com>
2020-04-28 18:55:07 +0300

    setup added

---
+++ setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+
+setup(
+  name="Jomini",
+  packages=["jomini"],
+  version="0.1.0",
+  license="MIT",
+  description="Historical battle simulation package for Python",
+  author="Ümit Kaan Usta",
+  author_email="u.kaanusta@gmail.com",
+  url="https://github.com/umitkaanusta/Jomini",
+  download_url="https://github.com/umitkaanusta/Jomini/archive/v0.1.0.tar.gz",
+  keywords=["military", "battle", "combat", "history", "simulation", "war", "historical", "attrition", "model"],
+  install_requires=[],
+  classifiers=[
+    "Development Status :: 3 - Alpha",
+    "Intended Audience :: Developers",
+    "Intended Audience :: Education",
+    "Topic :: Software Development :: Build Tools",
+    "License :: OSI Approved :: MIT License",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8"
+  ],
+)
